3d573a35d2476ec38d6ab2ac0c32cdf1a2201d66
stevieray8450 <stevieray8450@gmail.com>
2017-02-12 14:58:16 -0600

    first commit

--- BasicRectangle/Program.cs
+++ BasicRectangle/Program.cs
@@ -6,10 +6,47 @@ using System.Threading.Tasks;
 
 namespace BasicRectangle
 {
-    class Program
+    class Rectangle
     {
+        private double length { get; set; }
+        private double width { get; set; }
+        private double area { get; set; }
+
+        public Rectangle()
+        {
+            length = 0;
+            width = 0;
+            area = getArea(0, 0);
+        }
+
+        public Rectangle(double inputLength, double inputWidth)
+        {
+            length = inputLength;
+            width = inputWidth;
+            area = getArea(inputLength, inputWidth);
+        }
+
+        private double getArea(double inputLength, double inputWidth)
+        {
+            area = inputLength * inputWidth;
+            printInfo(inputLength, inputWidth, area);
+            return area;
+        }
+
+        public static void printInfo(double inputLength, double inputWidth, double area)
+        {
+            string rectangleDimensions = "Rectangle length: " + inputLength + "\nRectangle width: " + inputWidth + "\nRectangle area: " + area;
+            Console.WriteLine(rectangleDimensions);
+        }
+    }
+
+    class Program
+    { 
         static void Main(string[] args)
         {
+            Rectangle r1 = new Rectangle();
+            Rectangle r2 = new Rectangle(100.5, 25);
+            Console.ReadKey();
         }
     }
 }
